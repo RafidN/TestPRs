feb5526e41dccc75cd6ae635bf335f4d2a9b8e3e
Rafid Nasery <rafidn@outlook.com>
2026-08-25 13:57:15 +0000

    feature/add-new-67-function

--- src/main/java/com/neueda/leap/Main.java
+++ src/main/java/com/neueda/leap/Main.java
@@ -3,5 +3,15 @@ package com.neueda.leap;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world from the team's Sprint 1 project skeleton");
+        System.out.println("One year later. 67: " + OneYearLater(67));
+    }
+
+    public static int OneYearLater(int num)
+    {
+        if (num == 67)
+        {
+            return 78;
+        }
+        return 0;
     }
 }
